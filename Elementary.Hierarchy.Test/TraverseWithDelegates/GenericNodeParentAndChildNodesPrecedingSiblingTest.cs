55a6ce3253c9082ea350e358b29ae664c8f4d93d
Wolfgang Gross <github@text-mode.de>
2016-02-25 20:13:24 +0100

    Traverse preceding siblings with delegates

---
+++ Elementary.Hierarchy.Test/TraverseWithDelegates/GenericNodeParentAndChildNodesPrecedingSiblingTest.cs
@@ -0,0 +1,100 @@
+﻿namespace Elementary.Hierarchy.Test.TraverseWithDelegates
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Elementary.Hierarchy.Generic;
+
+    [TestFixture]
+    public class GenericNodeParentAndChildNodesPrecedingSiblingTest
+    {
+        private IEnumerable<string> GetChildNodes(string rootNode)
+        {
+            switch (rootNode)
+            {
+                case "rootNode":
+                    return new[] { "leftNode", "rightNode" };
+
+                case "leftNode":
+                    return Enumerable.Empty<string>();
+
+                case "rightNode":
+                    return new[] { "rightLeaf1", "rightLeaf2", "rightLeaf3" };
+            }
+            throw new InvalidOperationException();
+        }
+
+        private string GetParent(string startNode)
+        {
+            switch (startNode)
+            {
+                case "rootNode":
+                    return null;
+
+                case "leftNode":
+                case "rightNode":
+                    return "rootNode";
+
+                case "rightLeaf1":
+                case "rightLeaf2":
+                case "rightLeaf3":
+                    return "rightNode";
+            }
+            throw new InvalidOperationException();
+        }
+
+        [Test]
+        public void D_root_node_has_no_siblings_on_PrecedingSiblings()
+        {
+            // ACT
+
+            string[] result = "rootNode".PrecedingSiblings(this.GetParent, this.GetChildNodes).ToArray();
+
+            // ASSERT
+
+            Assert.IsFalse(result.Any());
+        }
+
+        [Test]
+        public void D_node_returns_left_sibling_on_PrecedingSiblings()
+        {
+            // ACT
+
+            string[] result = "rightNode".PrecedingSiblings(this.GetParent, this.GetChildNodes).ToArray();
+
+            // ASSERT
+
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual("leftNode", result.ElementAt(0));
+        }
+
+        [Test]
+        public void D_node_returns_no_siblings_on_PrecedingSiblings()
+        {
+            // ACT
+
+            string[] result = "leftNode".PrecedingSiblings(this.GetParent, this.GetChildNodes).ToArray();
+
+            // ASSERT
+
+            Assert.IsFalse(result.Any());
+        }
+
+        [Test]
+        public void D_node_returns_all_siblings_on_PrecedingSiblings()
+        {
+            // ACT
+
+            string[] result = "rightLeaf3".PrecedingSiblings(this.GetParent, this.GetChildNodes).ToArray();
+
+            // ASSERT
+
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("rightLeaf1", result.ElementAt(0));
+            Assert.AreEqual("rightLeaf2", result.ElementAt(1));
+        }
+    }
+}
--- Elementary.Hierarchy/HasParentAndChildNodesExtensions.cs
+++ Elementary.Hierarchy/HasParentAndChildNodesExtensions.cs
@@ -88,5 +88,32 @@ namespace Elementary.Hierarchy.Generic
 
             return getChildren(parent).SkipWhile(n => !n.Equals(startNode)).Skip(1);
         }
+
+        /// <summary>
+        /// Traverses the start nodes parent child nodes beginning with the first child of the parent untol the start node is reached.
+        /// The siblings are returned inside a <see cref="IEnumerable"/> of <see cref="TNode"/>.
+        /// The start node is not returned.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="IEnumerable"/> of <see cref="TNode"/> containing all visited nodes without the start node.
+        /// </returns>
+        /// <typeparam name="TNode">Type of the node, implements <see cref="IHasParentNode"/></typeparam>
+        /// <param name="startNode">reference to the node to start from</param>
+        /// <param name="getChildren">Delegate to retrieve a nodes children</param>
+        /// <param name="getParent">Delegate to retrieve a nodes parent</param>
+        public static IEnumerable<TNode> PrecedingSiblings<TNode>(this TNode startNode, Func<TNode, TNode> getParent, Func<TNode, IEnumerable<TNode>> getChildren)
+        {
+            if (getParent == null)
+                throw new ArgumentNullException(nameof(getParent));
+
+            if (getChildren == null)
+                throw new ArgumentNullException(nameof(getChildren));
+
+            var parent = getParent(startNode);
+            if (parent == null)
+                return Enumerable.Empty<TNode>();
+            
+            return getChildren(parent).TakeWhile(n => !n.Equals(startNode));
+        }
     }
 }
