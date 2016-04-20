b6ac64f1edc6053aab2592aff67834abe7dce76e
Wolfgang Groß <wolfgang.gross@siemens.com>
2016-04-20 10:20:22 +0200

    IHasDescandants(GetDescendatns(...,maxDepth): max depth is no longer optional

--- Elementary.Hierarchy/IHasDescendantNodes.cs
+++ Elementary.Hierarchy/IHasDescendantNodes.cs
@@ -16,6 +16,6 @@ namespace Elementary.Hierarchy
         /// <param name="depthFirst">Descend dethf first or breadth first</param>
         /// <param name="maxDepth">maximum deopths of descending 0 is always empty set, 1 are the children of this node and so on</param>
         /// <returns>Enumerable set of descendant nodes, in the order of visiting</returns>
-        IEnumerable<TNode> GetDescendants(bool depthFirst, int? maxDepth=null);
+        IEnumerable<TNode> GetDescendants(bool depthFirst, int maxDepth);
     }
 }
