3f0b6b2ac7cb2514575e88991f141df73d36c8ea
Wolfgang Groß <github@text-mode.de>
2016-10-14 19:38:20 +0200

    Copy AssemblyInfo fro old project

--- Elementary.Hierarchy/Properties/AssemblyInfo.cs
+++ Elementary.Hierarchy/Properties/AssemblyInfo.cs
@@ -1,15 +1,13 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Elementary.Hierarchy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Elementary Hierarchy provides structures and algorithms for developing of tree-like/hierarchical data structures.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Wolfgang Groß")]
 [assembly: AssemblyProduct("Elementary.Hierarchy")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -19,12 +17,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
