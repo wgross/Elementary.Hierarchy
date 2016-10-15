291ff5114833116abde7f567da154b0c3dd28df1
Wolfgang Groß <github@text-mode.de>
2016-10-15 09:56:18 +0200

    After nuget update package is built without warning

--- default.ps1
+++ default.ps1
@@ -21,18 +21,14 @@ $solutionFileNames = (Get-ChildItem -File $PSScriptRoot -Include *.sln)
 
 #region Nuget dependencies
 
-Task update_packages {
-    & $nuget update 
-} -precondition { Test-Path $nuget }
-
-Task restore_packages {
+Task restore_dependencies {
 
     # restore all nuget packages referenced by the current solution
     & $nuget restore $solutionFileNames
 
 } -precondition { Test-Path $nuget }
 
-Task clean_packages {
+Task clean_dependencies {
 
     Remove-Item $PSScriptRoot\packages -Recurse -ErrorAction SilentlyContinue
 
@@ -40,14 +36,36 @@ Task clean_packages {
 
 #endregion 
 
-Task clean {
+#region .Net Assemblies 
+
+Task clean_assemblies {
 
     & $msbuild $solutionFileNames /t:Clean /p:Configuration=Release
     & $msbuild $solutionFileNames /t:Clean /p:Configuration=Debug
-    
-    Remove-Item $PSScriptRoot\*.nupkg -ErrorAction SilentlyContinue
+}
 
-} -depends clean_packages
+#endregion
+
+#region Nuget packages
+
+Task clean_packages {
+     
+    Remove-Item $PSScriptRoot\*.nupkg -ErrorAction SilentlyContinue
+}
+
+Task build_packages {
+
+    # The package is built automatically by nuget in a Release configuration using the nuget package proerties extracted
+    # from the project file and following the package defineions of teh nuspec file lying next to the project file.
+    # As a result there should be nuget package containing the ElementaryHierarchy assembly and its generated XML documentation
+
+    & $nuget Pack (Resolve-path $PSScriptRoot\Elementary.Hierarchy\Elementary.Hierarchy.csproj) -Build -Prop "Configuration=Release" -Symbols -MSbuildVersion 14
+}
+
+Task rebuild_packages -depends clean_packages,build_packages
+
+#endregion
+
 
 Task build {
 
@@ -63,12 +81,12 @@ Task test {
 
 Task publish_local {
 
-    & $nuget Pack (Resolve-path $PSScriptRoot\Elementary.Hierarchy\Elementary.Hierarchy.csproj) -Build -Prop "Configuration=Release" -Symbols -MSbuildVersion 14
     
     # deploy to local package repo
     Copy-Item $PSScriptRoot\Elementary.Hierarchy.*.nupkg $localPackageSource
 
-} -precondition { Test-Path $nuget } -depends clean,test
+} -precondition { Test-Path $nuget } -depends test,clean_packages
 
-Task restore -depends restore_packages
+Task restore -depends restore_dependencies
+Task clean -depends clean_dependencies,clean_assemblies
 Task default -depends clean,build,test
