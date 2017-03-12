677bad3e750169ba30e454efcd7c3fbc048c399f
Wolfgang Groß <github@text-mode.de>
2017-03-12 11:48:50 +0100

    writing abstractions

---
+++ test/Elementary.Hierarchy.Test/EditUsingInterfaces/EditHierachyVisitorTEst.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elementary.Hierarchy.Test.EditUsingInterfaces
+{
+    [TestFixture]
+    public class EditHierachyVisitorTest
+    {
+        //[Test]
+        //public void 
+    }
+}
