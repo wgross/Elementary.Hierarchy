e6c2d5e774d1245ebf9c1b79391fb5ee2db494da
Wolfgang Gross <github@text-mode.de>
2017-04-15 23:23:17 +0200

    src projects are compiled for net461 too

--- clean.ps1
+++ clean.ps1
@@ -5,19 +5,36 @@ param(
 
     [Parameter(Position=1)]
     [ValidateSet("git_clean-xfd")]
-    [string[]]$Method
+    [string[]]$Method,
+
+    [Parameter()]
+    [switch]$Force
 )
+begin {
+    Import-Module -Name dev-tools
+}
 process {
+   
     if($Method -contains "git_clean-xfd") {
-        if($PsCmdlet.ShouldProcess("git clean -xfd")) {
-            "cleaning with git clean -xfd" | Write-Verbose
-        } else {
 
-            "cleaning with git clean -xfd -n" | Write-Verbose
-
-            git clean -xfd -n
+        if($Force.IsPresent) {
+            $gitForce = "-f"
         }
-        
-        #git clean -xfd $dryrun
+
+        $Path | Get-DotNetProjectItem -Process {
+
+            if($PsCmdlet.ShouldProcess("git clean -xfd")) {
+                
+                "cleaning with git clean -xd $gitForce $($_.Directory)" | Write-Verbose
+                    
+                git clean -xd $gitForce $_.Directory
+
+            } else {
+
+                "cleaning with git clean -xd -n $gitForce $($_.Directory)" | Write-Verbose
+
+                git clean -xd -n $gitForce $_.Directory
+            }
+        }
     } 
 }
