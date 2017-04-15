acf42a52feb49b1a8e8788378d8c173f4f7e236d
Wolfgang Gross <github@text-mode.de>
2017-04-15 23:06:22 +0200

    added a clean command

---
+++ clean.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding(SupportsShouldProcess)]
+param(
+    [Parameter(Position=0)]
+    $Path = $PWD,
+
+    [Parameter(Position=1)]
+    [ValidateSet("git_clean-xfd")]
+    [string[]]$Method
+)
+process {
+    if($Method -contains "git_clean-xfd") {
+        if($PsCmdlet.ShouldProcess("git clean -xfd")) {
+            "cleaning with git clean -xfd" | Write-Verbose
+        } else {
+
+            "cleaning with git clean -xfd -n" | Write-Verbose
+
+            git clean -xfd -n
+        }
+        
+        #git clean -xfd $dryrun
+    } 
+}
