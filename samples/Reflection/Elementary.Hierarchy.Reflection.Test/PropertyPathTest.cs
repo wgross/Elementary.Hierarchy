fa9e91bdae2c73bbed997380df777eb4cfc17810
Wolfgang Gross <github@text-mode.de>
2018-07-22 14:36:26 +0200

    path can be created fom propery access expression

---
+++ samples/Reflection/Elementary.Hierarchy.Reflection.Test/PropertyPathTest.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace Elementary.Hierarchy.Reflection.Test
+{
+    public class PropertyPathTest
+    {
+        [Fact]
+        public void Path_of_root_is_empty()
+        {
+            // ARRANGE
+
+            var left = new { };
+
+            // ACT
+
+            var result = left.PropertyPath(p => p);
+
+            // ASSERT
+
+            Assert.Equal(HierarchyPath.Create<string>(), result);
+        }
+
+        [Fact]
+        public void Path_of_property_is_its_name()
+        {
+            // ARRANGE
+
+            var left = new { a = 1 };
+
+            // ACT
+
+            var result = left.PropertyPath(p => p.a);
+
+            // ASSERT>
+
+            Assert.Equal(HierarchyPath.Create("a"), result);
+        }
+
+        [Fact]
+        public void Path_of_array_item_is_its_index()
+        {
+            // ARRANGE
+
+            var left = new { a = new[] { 1 } };
+
+            // ACT
+
+            var result = left.PropertyPath(p => p.a[0]);
+
+            // ASSERT>
+
+            Assert.Equal(HierarchyPath.Create("a","0"), result);
+        }
+    }
+}
--- samples/Reflection/Elementary.Hierarchy.Reflection/ObjectExtensions.cs
+++ samples/Reflection/Elementary.Hierarchy.Reflection/ObjectExtensions.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Elementary.Hierarchy.Reflection
 {
@@ -88,6 +89,43 @@ namespace Elementary.Hierarchy.Reflection
 
             if (!EqualityComparer<object>.Default.Equals(leftLeaf.Value, rightLeaf.Value))
                 compareResult.DifferentValues.Add(leftLeaf.Key);
+        }
+
+        public static HierarchyPath<string> PropertyPath<TRoot>(this TRoot root, Expression<Func<TRoot, object>> path)
+        {
+            return HierarchyPath.Create(PathSegments<TRoot>(root, path).Reverse());
+        }
+
+        public static IEnumerable<string> PathSegments<T>(T instance, Expression<Func<T, object>> access)
+        {
+            Expression current = access.Body;
+
+            while (current != null)
+            {
+                if (current is UnaryExpression && current.NodeType == ExpressionType.Convert)
+                {
+                    current = ((UnaryExpression)current).Operand;
+                }
+                else if (current is MemberExpression)
+                {
+                    var currentMemberExpression = current as MemberExpression;
+                    yield return currentMemberExpression.Member.Name;
+                    current = currentMemberExpression.Expression;
+                }
+                else if (current is BinaryExpression)
+                {
+                    var currentBinaryExpression = current as BinaryExpression;
+                    yield return ((ConstantExpression)currentBinaryExpression.Right).Value.ToString();
+                    current = currentBinaryExpression.Left;
+                }
+                else if (current is ParameterExpression)
+                {
+                    var parameterExpression = current as ParameterExpression;
+                    current = null;
+                }
+            }
+
+            yield break;
         }
     }
 }
